8f087c1bf4ffbaf2f263876fb3534aa6b5020b7b
dmmontoto <dmontoto@ucsd.edu>
2023-08-30 23:21:03 -0700

    added port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const apiRoutes = require('./public/routes/apiRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
